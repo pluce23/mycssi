66ece7d2dbdf3a8ff0bfcccb88fd28ac86fbdc99
PhilipBrian Luce <luce014@csusm.edu>
2019-07-24 15:55:33 -0700

    JS colorization lab

--- WeLearn/M2-JavaScript/L2-JSFunction/DOM/script.js
+++ WeLearn/M2-JavaScript/L2-JSFunction/DOM/script.js
@@ -6,3 +6,11 @@ myButton.addEventListener('click', (event) => {
   myButton.innerHTML = 'Liked!';
   myButton.style.backgroundColor = 'lightgreen';
 });
+
+myButton.addEventListener('mousemove', (event) =>{
+  let red = Math.round(Math.random()*255);
+  let green = Math.round(Math.random()*255);
+  let blue = Math.round(Math.random()*255);
+  myBox.style.backgroundColor = "rgb("+red+","+green+","+blue+")";
+
+});
